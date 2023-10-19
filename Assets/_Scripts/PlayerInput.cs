8feac0f59fe01f1fa22d505b306327a599d6b47c
MikeKuz <kuzovlev-mihail@mail.ru>
2023-10-19 10:50:06 +0300

    Initial commit

---
+++ Assets/_Scripts/PlayerInput.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+
+namespace Platformer2.Inputs
+{
+    //[RequireComponent(typeof(PlayerMovement))]
+
+    public class PlayerInput : MonoBehaviour
+    {
+        private Vector3 movment;
+        private PlayerMovement playerMovement;
+        public bool EPressed;
+
+        private void Awake()
+        {
+            playerMovement = GetComponent<PlayerMovement>();
+        }
+        void Update()
+        {
+            float horizontal = Input.GetAxis(GlobalStringVars.HORIZONTAL_AXIS);
+            float vertical = Input.GetAxis(GlobalStringVars.VERTICAZ_AXIS);
+
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                playerMovement.JumpCharacter();
+            }
+
+            if (Input.GetKey(KeyCode.E))
+            {
+                //TurnOffControlScript.EIsPressed = true;
+            }
+
+            else if (Input.GetKeyUp(KeyCode.E))
+            {
+                //TurnOffControlScript.EIsPressed = false;
+            }
+
+            movment = new Vector2(horizontal, 0).normalized;
+        }
+
+        private void FixedUpdate()
+        {
+            playerMovement.MoveCharacter(movment);
+        }
+    }
+}
---
+++ Assets/_Scripts/PlayerMovement.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Platformer2.Inputs
+{
+    //[RequireComponent(typeof(Rigidbody))]
+
+    public class PlayerMovement : MonoBehaviour
+    {
+        [SerializeField, Range(0, 20)] private float speed = 3.0f;
+        [SerializeField, Range(0, 20)] private float JumpPoewr = 2.0f;
+        private Rigidbody2D playerRigidbody2D;
+        static public bool underControl = true;
+        static bool jumpIsOn = true;
+
+        private void Awake()
+        {
+            playerRigidbody2D = GetComponent<Rigidbody2D>();
+            TurnOnUnderControl();
+        }
+
+        public void MoveCharacter(Vector2 movement)
+        {
+            if (underControl == true)
+            {
+                //playerRigidbody2D.AddRelativeForce(movement * speed);
+                Vector2 targetVelocity = new Vector2(movement.x*speed, movement.y);
+                playerRigidbody2D.velocity = targetVelocity;
+            }
+        }
+
+        public void JumpCharacter()
+        {
+            if (underControl == true)
+            {
+                if (jumpIsOn)
+                {
+                    playerRigidbody2D.AddForce(new Vector2(0, 100 * JumpPoewr));
+                    Debug.Log("пробел нажат");
+
+                }
+            }
+        }
+
+        static public void TurnOffUnderControl()
+        {
+            underControl = false;
+        }
+
+        static public void TurnOnUnderControl()
+        {
+            underControl = true;
+        }
+
+        static public void TurnOffJump()
+        {
+            jumpIsOn = false;
+        }
+
+        static public void TurnOnJump()
+        {
+            jumpIsOn = true;
+        }
+    }
+}
